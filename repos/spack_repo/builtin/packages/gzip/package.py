3b717d593f1de6249674d6e9a2c4a2f91dad1a18
Michael Kuhn <michael.kuhn@ovgu.de>
2026-09-24 00:43:11 +0200

    gzip: add 1.15

--- repos/spack_repo/builtin/packages/gzip/package.py
+++ repos/spack_repo/builtin/packages/gzip/package.py
@@ -16,10 +16,18 @@ class Gzip(AutotoolsPackage, GNUMirrorPackage):
 
     license("GPL-3.0-or-later")
 
+    version("1.15", sha256="545886cf57fa88a65e967fbf705903d7fcb2567c82c7342493e82e8d7b1a210b")
     version("1.14", sha256="613d6ea44f1248d7370c7ccdeee0dd0017a09e6c39de894b3c6f03f981191c6b")
     version("1.13", sha256="20fc818aeebae87cdbf209d35141ad9d3cf312b35a5e6be61bfcfbf9eddd212a")
 
-    depends_on("c", type="build")  # generated
+    depends_on("c", type="build")
+
+    # https://lists.gnu.org/archive/html/bug-gzip/2026-09/msg00031.html
+    patch(
+        "https://cgit.git.savannah.gnu.org/cgit/gzip.git/rawdiff/?id=b4ed8e73401968bcad749afb0e636dd3ec205ca7",
+        sha256="79dc82e4c747a7828e364d45a68a1049820e95d9a3772e585ff8a27146b44add",
+        when="@1.15",
+    )
 
     # Gzip makes a recursive symlink if built in-source
     build_directory = "spack-build"
